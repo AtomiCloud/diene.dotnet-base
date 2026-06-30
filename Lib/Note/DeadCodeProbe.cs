31d9da277821bcd1940870a629b3bd11f0872903
adelphi-liong <adelphi@atomi.cloud>
2026-06-30 13:04:08 -0700

    test: prove dead-code allows test and program usage

---
+++ Lib/Note/DeadCodeProbe.cs
@@ -0,0 +1,9 @@
+namespace AtomiCloud.DotnetBase.Lib.Note;
+
+public static class DeadCodeProbe
+{
+    public static string Describe()
+    {
+        return "dead-code proof test and program";
+    }
+}
