0ea6942c7bc52fe33b578b2aa4eb3ec6ebdc8084
adelphi-liong <adelphi@atomi.cloud>
2026-06-30 14:04:22 -0700

    test: prove dead-code catches test-only code

---
+++ Lib/Note/DeadCodeProof.cs
@@ -0,0 +1,9 @@
+namespace AtomiCloud.DotnetBase.Lib.Note;
+
+public static class DeadCodeProof
+{
+    public static string Describe()
+    {
+        return "dead-code proof test only rerun";
+    }
+}
---
+++ UnitTest/Note/DeadCodeProof_Describe.cs
@@ -0,0 +1,13 @@
+using AtomiCloud.DotnetBase.Lib.Note;
+using FluentAssertions;
+
+namespace AtomiCloud.DotnetBase.UnitTest.Note;
+
+public class DeadCodeProof_Describe
+{
+    [Fact]
+    public void It_should_return_the_probe_label()
+    {
+        DeadCodeProof.Describe().Should().Be("dead-code proof test only rerun");
+    }
+}
